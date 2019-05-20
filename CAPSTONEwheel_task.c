a38fc44252215d53cfba9fe6794b09e10a1ad7c5
Gron1275 <grennongurney@gmail.com>
2019-05-20 12:49:37 -0400

    Update CAPSTONEwheel_task.c
    Removed parts of program that are better suited for VEX IQ and added flashlight capability to zero out the green color of the conveyor belt

--- CAPSTONEwheel_task.c
+++ CAPSTONEwheel_task.c
@@ -1,20 +1,14 @@
-#pragma config(Sensor, in1,    colorSense,     sensorReflection)
 #pragma config(Sensor, dgtl1,  limit,          sensorTouch)
 #pragma config(Sensor, dgtl2,  startBump,      sensorTouch)
 #pragma config(Sensor, dgtl3,  led,            sensorLEDtoVCC)
-#pragma config(Sensor, dgtl4,  tiltLeft,     sensorTouch)
-#pragma config(Sensor, dgtl5,  tiltRight,      sensorTouch)
 #pragma config(Motor,  port2,           wheel,         tmotorServoContinuousRotation, openLoop, reversed)
 #pragma config(Motor,  port3,           conveyor,      tmotorServoContinuousRotation, openLoop)
 #pragma config(Motor,  port4,           mixer,         tmotorServoContinuousRotation, openLoop)
-#pragma config(Motor,  port5,           seesaw,        tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port5,           flashlight,    tmotorServoContinuousRotation, openLoop, reversed)  
 
 #include "jobs.h"
-//#include "tasks.c"
 
 int pauseLen = 0;
-
-
 
 task tickControl() //tickControl is made to regulate the frequency of m&ms being deposited onto the conveyor belt
 {
@@ -34,18 +28,11 @@ task main()
 	pauseLen = 3; //starting length of pause which can be changed using the two bump switches
 
 	startTask(tickControl); //Run tickControl
-	startTask(sortSide); //this turns the seesaw to either the left or right depending on if the m&m is green or red
 
 	while(true){ //always be runnin'
-		beltRun(63); //void to run the conveyor belt at a speed of 63
-		mix(20); //void to turn the stirring rod at a speed of 20
-		if(SensorValue[tiltLeft] == 1){ //this is an incremental regulator for the pauseLen variable
-			tilt(0); //tilt it left cup
-		}
-		if(SensorValue[tiltRight] == 1){ //this is an incremental regulator for the pauseLen variable
-			tilt(1); //tilt to right cup
-		}
-
-		delay(5); //scan for 5 ms cause idk why
+		turnLEDOn(flashlight);
+		beltRun(31.5); //void to run the conveyor belt at 1/4 full speed
+		mix(30); //void to turn the stirring rod at a speed of 30
+		delay(50); //scan for 50 ms 
 	}
 }
