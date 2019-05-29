dc5894f31a34b8ee95977d65cc9ece36cc084958
Gron1275 <grennongurney@gmail.com>
2019-05-29 13:04:37 -0400

    Update IQ_Jobs-new.h

--- IQ_Jobs-new.h
+++ IQ_Jobs-new.h
@@ -1,3 +1,15 @@
+void sensorInit()
+{
+	string x = getColorValue(colorSense);
+	setTouchLEDBlinkTime(feedbackLED, 5, 2.5);
+	setTouchLEDColor(feedbackLED, colorOrange);
+	if(getColorProximity(colorSense) <= 150){
+		//displayCenteredBigTextLine(2, y);
+		//displayCenteredBigTextLine(2, x);
+		displayBigTextLine(2, "%s", z);
+
+	}
+}
 void tilt(int side) //tilts the seesaw attatchment to either the left or the right depending on the m&m colo
 {
 	// 0 is equal to left
@@ -5,16 +17,18 @@ void tilt(int side) //tilts the seesaw attatchment to either the left or the rig
 	if(side == 0){ //if the side is equal to 0 (left), then tilt the seesaw to that side so m&m is sorted
 		//motor[seesaw] = -63; //the speed that the seesaw is tilted at is 63
 		setTouchLEDColor(feedbackLED, colorRed); //change LED to red
-		setMotorSpeed(seesaw, -90);
+		displayCenteredBigTextLine(2, "RED");
+		setMotorSpeed(seesaw, -60);
 		delay(1000);
-		motor[seesaw] = 0;
+		//motor[seesaw] = 0;
 	}
 	if(side == 1){ //if the side is equal to 1 (right), then tilt the seesaw to that side so m&m is sorted
 		//motor[seesaw] = 63; //the speed that the seesaw is tilted at
 		setTouchLEDColor(feedbackLED, colorGreen); //change led to green
-		setMotorSpeed(seesaw, 90);
+		displayCenteredBigTextLine(2, "GREEN");
+		setMotorSpeed(seesaw, 60);
 		delay(1000);
-		motor[seesaw] = 0;
+		//motor[seesaw] = 0;
 	} //void is pseudo-ish rn
 }
 task sortSide()
@@ -24,16 +38,19 @@ task sortSide()
 		// 0 is equal to left
 		// 1 is equal to right
 		if(getColorName(colorSense) == colorGreen){ //if the color is sensed as say, red, then tilt to the left and deposit the m&m
-			displayCenteredBigTextLine(2, "GREEN");
 			tilt(1); //void tilt to tilt seesaw left at a speed of 63
 		}
 		if(getColorName(colorSense) == colorRed){ //if the color is sensed as say, green, then tilt to the right and deposit the m&m
-			displayCenteredBigTextLine(2, "RED");
 			tilt(0); //void tilt to tilt seesaw right speed of 63
 		}
 		else{
-			displayCenteredBigTextLine(2, "Undefined");
-			setTouchLEDColor(feedbackLED, colorBlue);
+			displayCenteredBigTextLine(2, z);
+			//setTouchLEDColor(feedbackLED, x);
+
+
 		}
+		y = getColorMode(colorSense);
+		g = getColorMode(colorSense);
+		z = getColorName(colorSense);
 	}
 }
