e34d9bd678582992fd14e8d9346316ea034a8015
Gron1275 <grennongurney@gmail.com>
2019-05-31 12:33:07 -0400

    Update jobs.h

--- jobs.h
+++ jobs.h
@@ -1,10 +1,10 @@
 void mix(int speed){
-	while(true){								//runs stirring rod at a parameter defined speed
+	if(true){								//runs stirring rod at a parameter defined speed
 		motor[mixer] = speed;
 	}
 }
 void beltRun(int speed){
-	while(true){							  //runs conveyor belt at a parameter defined speed
+	if(true){							  //runs conveyor belt at a parameter defined speed
 		motor[conveyor] = speed;
 	}
 }
